79c8f9893034c08b897d83f131f2fbc33310120e
Dustin Ryan-Roepsch <dustin@Dustins-MacBook-Pro.local>
2018-02-08 08:46:11 -0800

    Refactor Keyboard to be general enough that extenders just need to specify the keyboard layout

--- src/main/java/com/dustinroepsch/korruct/Keyboards/Keyboard.java
+++ src/main/java/com/dustinroepsch/korruct/Keyboards/Keyboard.java
@@ -1,17 +1,36 @@
 package com.dustinroepsch.korruct.Keyboards;
 
+import java.util.List;
+
 /**
  * A Keyboard is a representation of a physical keyboard, used by SpellCorrector to evaluate
  * probability a word is a typo of another word.
  */
 public abstract class Keyboard {
 
+  private String[] keyTable = null;
+  private String keys = null;
+
   public int getMinDistance(String wordA, String wordB) {
     return 0; // TODO: Use Dynamic Programming to figure out the least-distance way to get from
     // wordA to wordB
   }
 
-  public abstract boolean isValidWord(String word);
+  public boolean isValidWord(String word) {
+    checkKeyboardSet();
+    return word.chars().mapToObj(i -> (char) i).allMatch(c -> keys.contains(String.valueOf(c)));
+  }
 
-  public abstract int getDistance(char charA, char charB);
+  public int getDistance(char charA, char charB) {
+    return 0; // TODO: Use keyTable to get the euclid distance between two keys.
+  }
+
+  abstract String[] getKeyboardLayout();
+
+  private void checkKeyboardSet() {
+    if (keyTable == null || keys == null) {
+      keyTable = getKeyboardLayout();
+      keys = List.of(keyTable).stream().distinct().reduce("", (a, s) -> a + s);
+    }
+  }
 }
--- src/main/java/com/dustinroepsch/korruct/Keyboards/QWERTYKeyboard.java
+++ src/main/java/com/dustinroepsch/korruct/Keyboards/QWERTYKeyboard.java
@@ -1,19 +1,8 @@
 package com.dustinroepsch.korruct.Keyboards;
 
-import java.util.List;
-
 public class QWERTYKeyboard extends Keyboard {
-  private static final String[] keyTable = {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
-  private static final String keys = List.of(keyTable).stream().reduce("", (a, s) -> a + s);
-
   @Override
-  public boolean isValidWord(String word) {
-    return word.chars().mapToObj(i -> (char) i).allMatch(c -> keys.contains(String.valueOf(c)));
-  }
-
-  @Override
-  public int getDistance(char charA, char charB) {
-    // TODO calculate distance between characters
-    return 0;
+  String[] getKeyboardLayout() {
+    return new String[] {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
   }
 }
